b69b54ca6b4670185d898f7056510eb5e5ff9c71
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2023-08-31 22:34:28 -0300

    Decoração em um CSS separado.

--- src/main.php
+++ src/main.php
@@ -8,8 +8,8 @@ $p1 = new Pessoa("Huginho", 12, "Cidade A.E. Carvalho(SP)");
 $p2 = new Pessoa("Agostinho", 57, "Baixada Fluminense(RJ)");
 $p3 = new Pessoa("Zezinho", 43, "Nova Cachoeirinha(MG)");
 $p4 = new Pessoa("Yellow", 15, "Viridian Forest (Kanto)");
+$p5 = new Pessoa("Pepper", 17, "Floresta de Squirrel's End (Hevera)");
 
-echo ("<style> .nicefont {font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;} td {padding: 10px; border: 2px black solid; text-align: center;} table {border: 2px black solid;} .primarytr {background-color: rgb(230, 230, 230);}</style>");
 echo ("<table>");
 
 echo("<tr class='primarytr'>");
@@ -17,28 +17,34 @@ echo("<td><p class='nicefont'> Nome"); echo("<td> Idade"); echo("<td> Localizaç
 echo("</tr></p>");
 
 echo("<tr>"); 
-echo ("<td><p class='nicefont'>Nome: "); echo $p1->nome;echo("</td>");
-echo ("<td>Idade: "),$p1->idade; echo ("</td>"); 
-echo("<td>Localização: "); echo $p1->local;echo ("</td>"); 
+echo ("<td><p class='nicefont'>"); echo $p1->nome;echo("</td>");
+echo ("<td> "),$p1->idade; echo ("</td>"); 
+echo("<td> "); echo $p1->local;echo ("</td>"); 
 echo("</p></tr>"); 
 
 echo("<tr>"); 
-echo ("<td><p class='nicefont'>Nome: "); echo $p2->nome;echo("</td>");
-echo ("<td>Idade: "),$p2->idade; echo ("</td>"); 
-echo("<td>Localização: "); echo $p2->local;echo ("</td>"); 
+echo ("<td><p class='nicefont'> "); echo $p2->nome;echo("</td>");
+echo ("<td> "),$p2->idade; echo ("</td>"); 
+echo("<td> "); echo $p2->local;echo ("</td>"); 
 echo("</p></tr>"); 
 
 echo("<tr>"); 
-echo ("<td><p class='nicefont'>Nome: "); echo $p3->nome;echo("</td>");
-echo ("<td>Idade: "),$p3->idade; echo ("</td>"); 
-echo("<td>Localização: "); echo $p3->local;echo ("</td>"); 
+echo ("<td><p class='nicefont'> "); echo $p3->nome;echo("</td>");
+echo ("<td> "),$p3->idade; echo ("</td>"); 
+echo("<td> "); echo $p3->local;echo ("</td>"); 
 echo("</p></tr>"); 
 
 echo("<tr>"); 
-echo ("<td><p class='nicefont'>Nome: "); echo $p4->nome;echo("</td>");
-echo ("<td>Idade: "),$p4->idade; echo ("</td>"); 
-echo("<td>Localização: "); echo $p4->local;echo ("</td>"); 
+echo ("<td><p class='nicefont'> "); echo $p4->nome;echo("</td>");
+echo ("<td> "),$p4->idade; echo ("</td>"); 
+echo("<td> "); echo $p4->local;echo ("</td>"); 
 echo("</p></tr>"); 
+
+echo("<tr>"); 
+echo ("<td><p class='nicefont'>"); echo $p5->nome;echo("</td>");
+echo ("<td>"),$p5->idade; echo ("</td>"); 
+echo("<td>"); echo $p5->local;echo ("</td>"); 
+echo("</p></tr>");
 
 echo ("</table>");
 ?>
